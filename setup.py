48379f2770c018d0148c0966c0ab6ffdfd0c5c7c
kriogman <jgonzalezf90@gmail.com>
2021-03-31 18:53:45 +0100

    Add long_description_content_type in setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(name='simpleweathercli',
       version=__version__,
       description='SimpleWeatherCLI is a simple command-line to find weather conditions in a city. It uses current and forecast data provided via API by https://openweathermap.org/. The purpose of this project is to create a simple example of a command-line interfaces using http://docopt.org/ and use Github Actions to build and upload the package to https://pypi.org/.',
       long_description=readme(),
+      long_description_content_type="text/markdown",
       classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
